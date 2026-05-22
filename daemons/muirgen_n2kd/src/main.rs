a21bec5117db9ba7224c18c3a4e24bead207f8e6
digimer <digital.mermaid@gmail.com>
2026-05-22 00:44:47 -0400

    Initial DB test access for the n2k ingest daemon.
    Signed-off-by: digimer <digital.mermaid@gmail.com>

--- daemons/muirgen_n2kd/src/main.rs
+++ daemons/muirgen_n2kd/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    // Load the .env DB access file
+    dotenvy::dotenv().ok();
+
+    // Pull the DATABASE_RULE out of the .env file
+    let db_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be defined in the .env file.");
+
+    println!("Accessing central database...");
+
+    // Connect using the URL from the .env file.
+    let pool = match PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await
+    {
+        Ok(p) => p, 
+        Err(e) => {
+            eprintln!("Access Failure! Error: [{}]", e);
+            std::process::exit(1);
+        }
+    };
+    
+        println!("Access granted. Verifying...");
+
+        let row: (i32,) = sqlx::query_as("SELECT 1;")
+            .fetch_one(&pool)
+            .await?;
+        
+        println!("Validated. Test responce: [{}]", row.0);
+
+        Ok(())
 }
